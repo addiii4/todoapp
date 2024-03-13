283c7147a90065a2464d218df5b7eb2b05f0fa22
Aditya Cheema <adity@MacBook-Pro.modem>
2024-03-13 11:05:42 +1000

    Fixed unique user issue

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -4,10 +4,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     email: { 
         type: String, 
-        required: true 
+        required: true,
+        unique: true 
     },
     username: { 
         type: String, 
+        unique: true 
     },
     password: { 
         type: String, 
