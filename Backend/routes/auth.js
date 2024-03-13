bdf4c59a3fa1340506814551973e933de90cf179
Aditya Cheema <adity@MacBook-Pro.modem>
2024-03-13 12:13:38 +1000

    Added bcryptjs to encrypt password

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,12 +1,14 @@
 //User authenication for signup and login
 const router  = require("express").Router();
 let User = require("../models/user");
+const bcrypt =  require('bcryptjs');
 
 //SIGN IN
 router.post("/auth", async(req,res) => {
     try {
        const {email, username, password} = req.body;
-       const user = new  User({email, username, password});
+       const hashPassword = bcrypt.hashSync(password);
+       const user = new  User({email, username, password: hashPassword});
        await user.save().then(() => res.status(200).json({user : user}));
     } catch (error) {
         res.status(400).json({message: "User already exists"})
