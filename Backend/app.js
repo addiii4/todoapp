e76319fabc4b54baad2badbfc4712b7fb3ac5e4c
addiii4 <adity@MacBook-Pro.modem>
2024-03-14 04:50:38 +1000

    Added sign in functionality

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 require("./connection/conn");
 const auth = require('./routes/auth');
+const task = require('./routes/task');
 app.use(express.json());       //Sending data in JSON format
 
 app.get("/", (req,res) => {
@@ -10,6 +11,7 @@ app.get("/", (req,res) => {
 })
 
 app.use("/api/v1", auth);
+app.use("/api/v2", task);
 
 app.listen(3000, () => {
     console.log("Server Running");
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,15 +1,34 @@
-//User authenication for signup and login
+//User authenication for signup and signin
 const router  = require("express").Router();
 let User = require("../models/user");
 const bcrypt =  require('bcryptjs');
 
-//SIGN IN
-router.post("/auth", async(req,res) => {
+//SIGN UP
+router.post("/signup", async(req,res) => {
     try {
-       const {email, username, password} = req.body;
-       const hashPassword = bcrypt.hashSync(password);
-       const user = new  User({email, username, password: hashPassword});
-       await user.save().then(() => res.status(200).json({user : user}));
+    const {email, username, password} = req.body;
+    const hashPassword = bcrypt.hashSync(password);
+    const user = new  User({email, username, password: hashPassword});
+    await user.save().then(() => res.status(200).json({user : user}));
+    } catch (error) {
+        res.status(400).json({message: "User already exists"})
+    }
+});
+
+//SIGN IN
+router.post("/signin", async(req,res) => {
+    try {
+    const user = await User.findOne({email: req.body.email});
+    if(!user){
+        res.status(404).json({message: "404: User not found :("})
+    } else{
+        let validPass = bcrypt.compareSync(req.body.password, user.password);
+        if (!validPass) {
+            res.status(401).json({message: "Invalid password"})
+        }
+        const { password, ...others} =  user._doc;
+        res.status(200).json(others)
+    }
     } catch (error) {
         res.status(400).json({message: "User already exists"})
     }
